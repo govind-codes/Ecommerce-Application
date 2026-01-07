f78be8e5d15941d1be6e5b203a72f2ab1ffaf183
govindsharma <govindsharma@localhost>
2026-01-07 21:19:39 +0530

    change 2: added update, delete, and find endpoints

--- 0-user-service/src/main/java/com/javaexpress/service/UserService.java
+++ 0-user-service/src/main/java/com/javaexpress/service/UserService.java
@@ -8,5 +8,6 @@ public interface UserService {
 	UserDto save(UserDto userDto);
 	UserDto updateUser(Integer userId, UserDto userDto);
 	UserDto findById(Integer userId);
+	void deleteUser(Integer userId);
 	
 }
--- 0-user-service/src/main/java/com/javaexpress/service/UserServiceImpl.java
+++ 0-user-service/src/main/java/com/javaexpress/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.javaexpress.service;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,25 +13,24 @@ import com.javaexpress.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
-	
+
 	@Autowired
 	UserRepository userRepository;
-	
+
 	@Autowired
 	UserMapper userMapper;
 
 	@Override
 	public UserDto save(UserDto userDto) {
-		
+
 		User user = userMapper.toUserEntity(userDto);
-		System.out.println("credentials: "+userDto.getCredential());
-		Credential credential= user.getCredential();
-		
-		System.out.println("credentials: "+credential.getUsername()+"' "+ credential.getPassword()+","+credential.getRoleBasedAuthority());
+		System.out.println("credentials: " + userDto.getCredential());
+		Credential credential = user.getCredential();
+
 		credential.setUser(user);
-		
+
 		userRepository.save(user);
-		
+
 		userDto = userMapper.toUserDto(user);
 		// TODO Auto-generated method stub
 		return userDto;
@@ -38,15 +39,35 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public UserDto updateUser(Integer userId, UserDto userDto) {
 		// TODO Auto-generated method stub
-		return null;
+
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new RuntimeException("user details not found for user Id: " + userId));
+
+		userMapper.updateUserFromDto(userDto, user);// this will update the exisitng record , won't create pass new
+													// userDto object, just update exisitng and make sure to use ignore
+													// id on mappoing annotation in userMapper class
+		System.out.println("uerrrrrr2: " + user.getUserId());
+		Credential credential = user.getCredential();
+
+		credential.setUser(user);
+		userRepository.save(user);
+
+		return userMapper.toUserDto(user);
 	}
 
 	@Override
 	public UserDto findById(Integer userId) {
 		// TODO Auto-generated method stub
-		return null;
+		return userRepository.findById(userId).map(userMapper::toUserDto)
+				.orElseThrow(() -> new RuntimeException("User details not found for this userid : " + userId));
 	}
+	
+	@Override
+	public void deleteUser(Integer userId) {
+		// TODO Auto-generated method stub
+		userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User details not found for this userid : " + userId));
+		userRepository.deleteById(userId);
+	}
+	
 
-	
-	
 }
