cd82db8a65932723abb54d4a553863533389af66
Govind Sharma <govindsharma@Govinds-MacBook-Air.local>
2026-01-08 22:54:38 +0530

    [Change1 Created cart service]

---
+++ 2-cart-service/src/main/java/com/javaexpress/dto/CartItemResponseDto.java
@@ -0,0 +1,14 @@
+package com.javaexpress.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CartItemResponseDto {
+
+	private Long id;
+	private Long userId;
+	private Long productId;
+	private Integer quantity;
+}
